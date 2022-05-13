f446f64d02459c957331195c6e360b8f969d7e10
Omid Estaji <estaji.work@gmail.com>
2022-05-13 12:47:07 +0430

    Add more unit test for models

--- resume/tests/test_models.py
+++ resume/tests/test_models.py
@@ -1,6 +1,14 @@
 from django.test import TestCase
 from datetime import date
-from resume.models import Job
+from resume.models import (
+    Job,
+    Education,
+    TechSkill,
+    SoftSkill,
+    Language,
+    Jumbotron,
+    Configuration,
+)
 
 
 class JobModelTests(TestCase):
@@ -58,18 +66,167 @@ class JobModelTests(TestCase):
         self.assertEqual(item.location, 'USA')
         self.assertEqual(item.summary, 'Lead all tech departments')
 
+
+class EducationModelTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Education.objects.create(
+            university='Stanford',
+            level='PhD',
+            title='Computer Security',
+            description='protection of computer systems',
+            start=date.today(),
+            end=None,
+        )
+        Education.objects.create(
+            start=date.today(),
+            end=date.today()
+        )
+
     def test_add_education(self):
         """Test add a education"""
-        pass
+        item = Education.objects.get(id=1)
+
+        self.assertEqual(item.title, 'Computer Security')
+        self.assertEqual(item.university, 'Stanford')
+        self.assertEqual(item.level, 'PhD')
+        self.assertEqual(item.description, 'protection of computer systems')
 
     def test_short_start_year(self):
         """Test education start date shows year"""
-        pass
+        item = Education.objects.get(id=1)
+        date1 = Education.short_start(item)
+        for_date2 = item.start
+        date2 = for_date2.strftime("%Y")
+
+        self.assertEqual(date1, date2)
 
     def test_modified_end_year_present(self):
         """Test education end field returns PRESENT when is empty"""
-        pass
+        item = Education.objects.get(id=1)
+        end_field = Education.modified_end(item)
+
+        self.assertEqual(end_field, 'Present')
 
     def test_modified_end_year(self):
         """Test education end field returns modified date when is NOT empty"""
-        pass
+        item = Education.objects.get(id=2)
+        date1 = Education.modified_end(item)
+        for_date2 = item.end
+        date2 = for_date2.strftime("%Y")
+
+        self.assertEqual(date1, date2)
+
+
+class TechSkillModelTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        TechSkill.objects.create(
+            title='Network Administration',
+            certs='CCIE',
+            details='the highest networking certifications in the industry.',
+            order=1,
+        )
+
+    def test_add_techskill(self):
+        """Test add a technical skill"""
+        item = TechSkill.objects.get(id=1)
+
+        self.assertEqual(item.title, 'Network Administration')
+        self.assertEqual(item.certs, 'CCIE')
+        self.assertEqual(
+            item.details,
+            'the highest networking certifications in the industry.'
+        )
+        self.assertEqual(item.order, 1)
+
+
+class SoftSkillModelTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        SoftSkill.objects.create(
+            title='Leadership',
+            order=2,
+        )
+
+    def test_add_softskill(self):
+        """Test add a soft skill"""
+        item = SoftSkill.objects.get(id=1)
+
+        self.assertEqual(item.title, 'Leadership')
+        self.assertEqual(item.order, 2)
+
+
+class LanguageModelTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Language.objects.create(
+            title='English',
+            level='IELTS 9',
+            order=3,
+        )
+
+    def test_add_language(self):
+        """Test add a language"""
+        item = Language.objects.get(id=1)
+
+        self.assertEqual(item.title, 'English')
+        self.assertEqual(item.level, 'IELTS 9')
+        self.assertEqual(item.order, 3)
+
+
+class JumbotronModelTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Jumbotron.objects.create(
+            greeting='Welcome to my site',
+            title='My full name',
+            description='This is my personal website',
+            email='me@test.com',
+        )
+
+    def test_add_jumbotron(self):
+        """Test add a jumbotron"""
+        item = Jumbotron.objects.get(id=1)
+
+        self.assertEqual(item.greeting, 'Welcome to my site')
+        self.assertEqual(item.title, 'My full name')
+        self.assertEqual(item.description, 'This is my personal website')
+        self.assertEqual(item.email, 'me@test.com')
+
+
+class SocialAccountModelTests(TestCase):
+
+    pass
+
+
+class ConfigurationModelTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Configuration.objects.create(
+            site_title='My site',
+            title='My full name',
+            description='This is my personal website',
+            robots='follow',
+            author='Mr Test',
+            keywords='personal blog, tag',
+            copyr='All rights reserved',
+        )
+
+    def test_add_configuration(self):
+        """Test add a configuration"""
+        item = Configuration.objects.get(id=1)
+
+        self.assertEqual(item.site_title, 'My site')
+        self.assertEqual(item.title, 'My full name')
+        self.assertEqual(item.description, 'This is my personal website')
+        self.assertEqual(item.robots, 'follow')
+        self.assertEqual(item.author, 'Mr Test')
+        self.assertEqual(item.keywords, 'personal blog, tag')
+        self.assertEqual(item.copyr, 'All rights reserved')
