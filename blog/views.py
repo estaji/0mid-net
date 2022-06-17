cd3d87075da5082350e0163a9cdfbbdf0c8f081a
Omid Estaji <estaji.work@gmail.com>
2022-06-17 08:42:29 +0430

    Edit title tag in all blog pages

--- blog/views.py
+++ blog/views.py
@@ -42,6 +42,7 @@ class TagView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tag'] = tag
+        context['settings'] = Configuration.objects.first()
         return context
 
 
