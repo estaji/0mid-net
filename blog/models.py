93f17cf5f4f0a42a7952eca2097cd2e6aac37e69
Omid Estaji <estaji.work@gmail.com>
2022-06-30 10:39:33 +0430

    Add meta tags to tagslist and every tag page

--- blog/models.py
+++ blog/models.py
@@ -25,6 +25,16 @@ class Tag(models.Model):
         blank=True,
         verbose_name='Meta description tag',
     )
+    keywords = models.CharField(
+        max_length=100,
+        blank=True,
+        verbose_name='Meta keywords tag',
+    )
+    robots = models.CharField(
+        max_length=60,
+        blank=True,
+        verbose_name='Meta robots tag',
+    )
 
     class Meta:
         verbose_name = 'Tag'
