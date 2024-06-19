8e8531886daede599a03e92555148d4e274a8352
Omid Estaji <estaji.work@gmail.com>
2024-06-19 17:52:31 +0330

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,19 +1,55 @@
 #!/bin/bash
-timestamp=$(date +%F-%H%M%S)
-echo --- backup started at $timestamp ---
 
-echo dumping database...
-docker exec mariadb bash -c 'mariadb-dump -u$SITE_DB_USER -p$SITE_DB_PASSWORD $SITE_DB_NAME' > $SITE_DB_NAME-$timestamp.sql
+############################################
+#Description:	Backup 0mid server files
+############################################
 
-echo nginx config backup is creating...
-cp /etc/nginx/sites-available/0mid.net ./0mid.net-nginx-$timestamp
+# variables
+DATE_AND_TIME=$(date +%F-%H-%M-%S)
+SYSLOG_FACILITY_NAME=local0.info
+LOGGER_TAG=backup-script
 
-echo media and static files are archiving...
-tar -czf web_media-$timestamp.tar.gz /var/www/web_media
-tar -czf web_static-$timestamp.tar.gz /var/www/web_static
-
-echo "put all files in a .tar.gz file..."
-tar -czf backup-$timestamp.tar.gz *$timestamp*
-rm 0middb-$timestamp.sql 0mid.net-nginx-$timestamp web_media-$timestamp.tar.gz web_static-$timestamp.tar.gz
-
-printf "\n--- backup finished ---\n"
+# functions
+dump_dockerized_mariadb() {
+    docker exec mariadb bash -c 'mariadb-dump -u$SITE_DB_USER -p$SITE_DB_PASSWORD $SITE_DB_NAME' > $SITE_DB_NAME-$DATE_AND_TIME.sql
+    if [ $? -ne 0 ]; then
+        logger -p $SYSLOG_FACILITY_NAME -t "$LOGGER_TAG" "Error: Dumping database failed!"
+    fi
+    logger -p $SYSLOG_FACILITY_NAME -t "$LOGGER_TAG" "Database dumped successfully."
+}
+backup_nginx_configs() {
+    tar -zcf nginx-configs-$DATE_AND_TIME.tar.gz -C / etc/nginx/sites-available etc/nginx/nginx.conf etc/nginx/conf.d
+    if [ $? -ne 0 ]; then
+        logger -p $SYSLOG_FACILITY_NAME -t "$LOGGER_TAG" "Error: Nginx backuping failed!"
+    fi
+    logger -p $SYSLOG_FACILITY_NAME -t "$LOGGER_TAG" "Nginx configurations backup successfully."
+}
+archive_website_static_files() {
+    tar -czf website_static_and_media-$DATE_AND_TIME.tar.gz -C / var/www/web_media var/www/web_static
+    if [ $? -ne 0 ]; then
+        logger -p $SYSLOG_FACILITY_NAME -t "$LOGGER_TAG" "Error: Archive website static and media files failed!"
+    fi
+    logger -p $SYSLOG_FACILITY_NAME -t "$LOGGER_TAG" "Website static and media files archived successfully."
+}
+compress_all() {
+    tar --remove-files -czf backup-$DATE_AND_TIME.tar.gz *$DATE_AND_TIME*
+    if [ $? -ne 0 ]; then
+        logger -p $SYSLOG_FACILITY_NAME -t "$LOGGER_TAG" "Error: Compressing files failed!"
+    fi
+    logger -p $SYSLOG_FACILITY_NAME -t "$LOGGER_TAG" "Files are compressed in one file successfully."
+}
+backup_monitoring() {
+    tar -czf monitoring-stack-$DATE_AND_TIME.tar.gz -C / home/omid/monitoring-dockerized/docker-compose.yaml home/omid/monitoring-dockerized/alertmanager/config home/omid/monitoring-dockerized/prometheus/prometheus.yml home/omid/monitoring-dockerized/prometheus/rules
+    if [ $? -ne 0 ]; then
+        logger -p $SYSLOG_FACILITY_NAME -t "$LOGGER_TAG" "Error: Backup monitoring-stack files failed!"
+    fi
+    logger -p $SYSLOG_FACILITY_NAME -t "$LOGGER_TAG" "Monitoring-stack files backuped successfully."
+}
+# main
+logger -p $SYSLOG_FACILITY_NAME -t "$LOGGER_TAG" "######## Backup script started ########"
+dump_dockerized_mariadb
+backup_nginx_configs
+archive_website_static_files
+backup_monitoring
+compress_all
+logger -p $SYSLOG_FACILITY_NAME -t "$LOGGER_TAG" "######## Backup script finished ########"
