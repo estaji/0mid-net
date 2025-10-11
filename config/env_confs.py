fae9fdd7e95b1c08c86d9920db5e59882e4407fb
Omid <estaji.work@gmail.com>
2025-10-11 22:00:26 +0330

    Init fresh project

---
+++ config/env_confs.py
@@ -0,0 +1,10 @@
+from os import environ
+
+class EnvConfig:
+    SECRET_KEY = environ.get("SITE_SECRET_KEY")
+    DB_NAME = environ.get("SITE_DB_NAME")
+    DB_USER = environ.get("SITE_DB_USER")
+    DB_PASSWORD = environ.get("SITE_DB_PASSWORD")
+    DB_HOST = environ.get("SITE_DB_HOST")
+    DB_PORT = environ.get("SITE_DB_PORT")
+    SITE_ENV = environ.get("SITE_ENV")
---
+++ config/settings/prod.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["0mid.net", "www.0mid.net", "127.0.0.1", "localhost"]
+
+CSRF_TRUSTED_ORIGINS = ["https://*.0mid.net", "https://*.127.0.0.1"]
