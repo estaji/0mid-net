9cbf2f311d6e101d11abd86530c12d064afeece3
estaji.omid <estaji.omid@tara360.ir>
2025-10-12 09:20:39 +0330

    Add menu and some confs to models

--- resume/models.py
+++ resume/models.py
@@ -150,6 +150,21 @@ class Configuration(models.Model):
         blank=True,
         verbose_name="Title tag",
     )
+    favicon = models.ImageField(
+        upload_to="favicon/",
+        verbose_name="Favicon",
+        blank=True,
+    )
+    copyright_text = models.CharField(
+        max_length=160,
+        blank=True,
+        verbose_name="Copyright text",
+    )
+    google_analytics = models.CharField(
+        max_length=100,
+        blank=True,
+        verbose_name="Google Analytics ID",
+    )
     description = models.CharField(
         max_length=160,
         blank=True,
@@ -180,3 +195,52 @@ class Configuration(models.Model):
         blank=True,
         verbose_name="X username for X site tag",
     )
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Title")
+    url = models.URLField(
+        max_length=100,
+        default="https://#",
+        verbose_name="URL",
+        blank=True,
+    )
+    order = models.IntegerField(unique=True, verbose_name="Order Number")
+
+    DROPDOWN = "DD"
+    NORMAL = "N"
+    DISABLED = "DI"
+    TYPE_CHOICES = [
+        (DROPDOWN, "Has submenu"),
+        (NORMAL, "Don't have submenu"),
+        (DISABLED, "Is deactivate"),
+    ]
+    icon_type = models.CharField(
+        max_length=2,
+        choices=TYPE_CHOICES,
+        default=NORMAL,
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["order"]
+
+
+class SubMenu(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Title")
+    url = models.URLField(
+        max_length=100,
+        default="https://#",
+        verbose_name="URL",
+        blank=True,
+    )
+    parent = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    order = models.IntegerField(unique=True, verbose_name="Order Number")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["order"]
