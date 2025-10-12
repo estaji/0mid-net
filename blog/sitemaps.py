059515fbb1492c44c2176db7bbe407b29aa2876a
Omid <estaji.work@gmail.com>
2025-10-12 21:01:37 +0330

    Add sitemap

---
+++ blog/sitemaps.py
@@ -0,0 +1,49 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+from blog.models import Article, Tag
+
+
+class StaticSitemap(Sitemap):
+    """Create sitemap for static pages"""
+
+    changefreq = "monthly"
+    priority = 1.0
+    protocol = "https"
+
+    def items(self):
+        return ["resume", "blog:home", "blog:tagslist"]
+
+    def location(self, item):
+        return reverse(item)
+
+
+class ArticleSitemap(Sitemap):
+    """Create sitemap for articles"""
+
+    changefreq = "monthly"
+    priority = 0.7
+    protocol = "https"
+
+    def items(self):
+        return Article.objects.published()
+
+    def location(self, obj):
+        return "/blog/%s" % (obj.slug)
+
+    def lastmod(self, obj):
+        return obj.updated
+
+
+class TagSitemap(Sitemap):
+    """Create sitemap for tag page"""
+
+    changefreq = "monthly"
+    priority = 0.5
+    protocol = "https"
+
+    def items(self):
+        return Tag.objects.all()
+
+    def location(self, obj):
+        return "/blog/tag/%s" % (obj.slug)
--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
 
 from resume.views import ResumeView
+from blog.sitemaps import ArticleSitemap, StaticSitemap, TagSitemap
 
+
+sitemaps = {
+    "static": StaticSitemap(),
+    "tag": TagSitemap(),
+    "article": ArticleSitemap(),
+}
 
 urlpatterns = [
     path(
@@ -14,6 +22,12 @@ urlpatterns = [
     ),
     path('admin/', admin.site.urls, name="admin-panel"),
     path("blog/", include("blog.urls")),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
